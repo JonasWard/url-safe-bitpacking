a6ad48c9adc88bac5f3887851ba116832674404c
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-10-04 21:58:18 +0200

    type mapping

--- src/enums/dataTypes.ts
+++ src/enums/dataTypes.ts
@@ -5,3 +5,18 @@ export enum DataType {
   INT,
   FLOAT,
 }
+
+export const getDataTypeName = (type: DataType) => {
+  switch (type) {
+    case DataType.VERSION:
+      return 'VERSION';
+    case DataType.BOOLEAN:
+      return 'BOOLEAN';
+    case DataType.ENUM:
+      return 'ENUM';
+    case DataType.INT:
+      return 'INT';
+    case DataType.FLOAT:
+      return 'FLOAT';
+  }
+};
