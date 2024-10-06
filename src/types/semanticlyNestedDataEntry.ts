8fce06c32f1c3bd00e4483c667649a06bebbc60f
JonasWard <43600893+JonasWard@users.noreply.github.com>
2024-10-06 23:16:00 +0200

    semantically nested values type

--- src/types/semanticlyNestedDataEntry.ts
+++ src/types/semanticlyNestedDataEntry.ts
@@ -3,3 +3,7 @@ import { DataEntry } from './dataEntry';
 export type SemanticlyNestedDataEntry = {
   [key: string]: SemanticlyNestedDataEntry | DataEntry;
 };
+
+export type SemanticlyNestedValues = {
+  [key: string]: SemanticlyNestedValues | boolean | number | string;
+};
