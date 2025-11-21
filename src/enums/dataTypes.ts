36873fb502189118d82dfa5207c79a8e8078682b
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-21 14:17:45 +0100

    feat: ComplexDataTypes

--- src/enums/dataTypes.ts
+++ src/enums/dataTypes.ts
@@ -1,3 +1,5 @@
-const DataTypeValues = ['VERSION', 'BOOLEAN', 'ENUM', 'INT', 'FLOAT', 'ENUM_ARRAY'] as const;
+export const DataTypeValues = ['VERSION', 'BOOLEAN', 'ENUM', 'INT', 'FLOAT', 'ENUM_ARRAY'] as const;
+export const ComplexDataValues = ['OPTIONAL', 'ENUM_OPTIONS', 'ARRAY'] as const;
 
 export type DataType = (typeof DataTypeValues)[number];
+export type ComplexDataType = (typeof ComplexDataValues)[number];
