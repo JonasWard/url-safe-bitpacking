b65e66eb28677f77255188df1f119457b027c14c
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-11-21 15:06:13 +0100

    feat: new definition for enum objects

---
+++ src/factory/utils.ts
@@ -0,0 +1,10 @@
+/**
+ * Method to get the max and mapping from the options
+ * @param options - `any[] | string | number` the options to get the max and mapping from
+ * @returns `{ max: number; mapping: any[] }` the max and mapping
+ */
+export const getOptionsFromEnumOptions = (options: any[] | string | number): { max: number; mapping: any[] } => {
+  if (typeof options === 'string') return { max: options.length - 1, mapping: options.split(',').map(Number) };
+  if (typeof options === 'number') return { max: options, mapping: Array.from({ length: options + 1 }, (_, i) => i) };
+  return { max: options.length - 1, mapping: options };
+};
