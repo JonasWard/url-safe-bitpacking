aff00a013c5240320ba96d4c708cea22634fcc35
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-12-26 22:53:16 +0100

    feat: ObjectData -> 'pass through' collection

---
+++ src/factory/objectFactory.ts
@@ -0,0 +1,13 @@
+import { DataEntry, ObjectDataEntry } from '../types';
+
+export type ObjectFactory = (descriptor: DataEntry[], name?: string) => ObjectDataEntry;
+
+export const create: ObjectFactory = (descriptor, name = 'an object') => {
+  return {
+    type: 'OBJECT',
+    descriptor,
+    value: descriptor,
+    name,
+    stateBits: 0
+  };
+};
