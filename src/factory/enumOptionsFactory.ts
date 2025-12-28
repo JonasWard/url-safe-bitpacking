0d42bc5807800a6bd9944bf5d5600c2550d06af2
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-12-28 10:24:33 +0100

    chore: type fix for the getCopy method, checking whether all data in the Descriptor are ObjectDataEntry

--- src/factory/enumOptionsFactory.ts
+++ src/factory/enumOptionsFactory.ts
@@ -16,13 +16,15 @@ export const create: EnumOptionsFactory = (descriptor, defaultState = 0, name = 
   if (descriptor.length < 2) throw new Error('descriptor must have at least two entries');
   if (descriptor.length - 1 < defaultState)
     throw new Error('defaultState must be less than the length of the descriptor');
+  if (descriptor.some((v) => v !== null && v.type !== 'OBJECT'))
+    throw new Error('descriptors must be either ObjectDataEntry or null');
 
   // options are derived from the descriptor
   const { max, mapping } = getEnumMaxAndMappingFromOptions(descriptor.map((v, i) => (v ? v.name : i)));
   if (max !== descriptor.length - 1) throw new Error('max must be equal to the length of the descriptor - 1');
 
   return {
-    value: getCopy(descriptor[defaultState]),
+    value: getCopy(descriptor[defaultState] as ObjectDataEntry | null) as ObjectDataEntry | null,
     descriptor,
     name,
     mapping,
